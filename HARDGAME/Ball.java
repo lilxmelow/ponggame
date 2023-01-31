4b026882c1e0a438beb43228821e0421e68362f1
lilxmelow <119453720+lilxmelow@users.noreply.github.com>
2023-01-31 19:17:38 +0100

    Update Ball.java

--- HARDGAME/Ball.java
+++ HARDGAME/Ball.java
@@ -2,17 +2,19 @@ package HARDGAME;
 
 
 public class Ball {
-
+//variables
+//size of the ball
 private final int WIDTH = 10;
 private final int HEIGHT = 10;
 
-
+//position of the ball
 private int x;
 private int y;
 
+//speed of the ball
 private int speed;
 
-
+//constructor
 public Ball(int s){
 	x = 300;
 	y = 200;
