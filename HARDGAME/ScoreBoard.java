7b659da8cf503cbeb3e186ca794523eac7d81d39
lilxmelow <119453720+lilxmelow@users.noreply.github.com>
2023-01-31 19:20:04 +0100

    Update ScoreBoard.java

--- HARDGAME/ScoreBoard.java
+++ HARDGAME/ScoreBoard.java
@@ -9,30 +9,31 @@ import javax.swing.JPanel;
 
 
 public class ScoreBoard extends JPanel{
-
+//variables
 private int p1_score;
 private int p2_score;
 
-private final int WINNING_SCORE = 10;
+//final score
+private final int WINNING_SCORE = 2;
 
-
+//determine the winners
 private boolean p1_wins = false;
 private boolean p2_wins = false;
 
 private JLabel score1, score2;
 
-
+//constructor
 public ScoreBoard(){
 	p1_score = 0;
-	p2_score = 0;
+	p2_score = -1;
 	
 	score1 = new JLabel();
-	score1.setText("P1: " + p1_score);
+	score1.setText("" + p1_score);
 	score1.setFont(new Font("Monospaced",Font.BOLD,25));
 	score1.setForeground(Color.WHITE);
 	
 	score2 = new JLabel();
-	score2.setText("P2: " + p2_score);
+	score2.setText("" + p2_score);
 	score2.setFont(new Font("Monospaced",Font.BOLD,25));
 	score2.setForeground(Color.WHITE);
 	
@@ -42,21 +43,45 @@ public ScoreBoard(){
 }
 
 public void pointP1(){
+	
 	p1_score++;
+	//if player 1 score enough points to win
+	
 	if(p1_score == WINNING_SCORE){
 		p1_wins = true;
+		score1.setText("Right Wins!");
+		score2.setText("");
+		
+		
 	}
-	score1.setText("P1: " + p1_score);
+	
+	else{
+		score1.setText(p1_score + " ");
+	}
+	
 }
 
 public void pointP2(){
 	p2_score++;
+	//if player 2 scores enough points to win
 	
 	if(p2_score == WINNING_SCORE){
 		p2_wins = true;
+		score2.setText("Left Wins!");
+		score1.setText("");
+		
 	}
-	score2.setText("P2: " + p2_score);
-}
+	
+	
+	else{
+		score2.setText(" " + p2_score);
+		
+	}
+	
+	}
+	
+
+
 
 public int getScoreP1(){
 	return p1_score;
